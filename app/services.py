5211a29e6946ab6a07e76fe9d548e511a0be1715
meghana367 <smeghana021@gmail.com>
2026-04-05 23:43:01 +0530

    Change TransactionType references to lowercase
    Updated TransactionType references to use lowercase.

--- app/services.py
+++ app/services.py
@@ -3,17 +3,14 @@ from sqlalchemy import func
 from app.models import Transaction, TransactionType
 
 def get_financial_summary(db: Session):
-    # Change TransactionType.INCOME to TransactionType.income
     total_income = db.query(func.sum(Transaction.amount)).filter(
         Transaction.type == TransactionType.income
     ).scalar() or 0.0
 
-    # Change TransactionType.EXPENSE to TransactionType.expense
     total_expenses = db.query(func.sum(Transaction.amount)).filter(
         Transaction.type == TransactionType.expense
     ).scalar() or 0.0
 
-    # Category breakdown (This part should be fine, but check it)
     category_totals = db.query(
         Transaction.category, 
         func.sum(Transaction.amount)
